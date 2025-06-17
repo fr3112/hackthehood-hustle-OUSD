efbb1b9118fabd7708d3683008fcb83e23a8184e
fr3112 <erikafernandaramosderas@Erikas-MacBook-Pro.local>
2025-06-17 10:15:42 -0700

    practicing variables

---
+++ lab2.py
@@ -0,0 +1,21 @@
+#  Lab  2
+
+# Step 1
+name = "Erika Ramos"
+
+# Step 2
+nickname = "Fer"
+
+# Step  3
+age = 17.0
+
+# Step 4
+used_python = False
+
+# Step 5 
+hobbies = ["knit","listening to music", "drawing","paint with watercolor"]
+
+# Step 6
+favorite_things = {"Movie": "The perks of being a wallflower", "Food": "Lasagna","Hobby": "Listening to music", "Color": "Green", "Number": 7, "Animal": "Raccoon"}
+
+# optional
